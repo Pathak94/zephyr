9c699e08b80b2473e445dfb3bf566fda4bd286ff
vaishali <vaishali@electronut.in>
2019-04-03 17:35:57 +0530

    ltr driver complete

--- drivers/sensor/ltr329als01/ltr329als01.c
+++ drivers/sensor/ltr329als01/ltr329als01.c
@@ -1,71 +1,56 @@
+/** @file module.c
+ *
+ * @brief A description of the module’s purpose. 
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2018 Electronut Labs.
+ * All rights reserved. 
+*/
 
-#include <sensor.h>
-#include <kernel.h>
-#include <device.h>
-#include <init.h>
-#include <string.h>
-#include <misc/byteorder.h>
-#include <misc/__assert.h>
-#include <logging/log.h>
 #include "ltr329als01.h"
-#include <i2c.h>
-
-#define LOG_LEVEL CONFIG_SENSOR_LOG_LEVEL
-#include <logging/log.h>
-LOG_MODULE_REGISTER(ltr329als01);
-
-#define RESERVED -1
 
 struct ltr329als01_data {
 	struct device *i2c_dev;
 	uint16_t lux_val;
 };
 
-static int write_bytes(struct device *i2c_dev, u16_t addr,
-		       u8_t *data, u32_t num_bytes)
+/**
+ * @brief function for write a byte
+ * 
+ * @return 0 for success
+ */
+static int write_byte(struct device *i2c_dev, u8_t addr, u8_t data)
 {
-	u8_t wr_addr;
-	struct i2c_msg msgs[2];
+	struct i2c_msg msg;
 
-	/* FRAM address */
-	wr_addr = addr;
-
+	u8_t buf[2] = { addr, data };
 	/* Setup I2C messages */
 
 	/* Send the address to write to */
-	msgs[0].buf = &wr_addr;
-	msgs[0].len = 1;
-	msgs[0].flags = I2C_MSG_WRITE;
+	msg.buf = &buf[0];
+	msg.len = 2;
+	msg.flags = I2C_MSG_WRITE | I2C_MSG_STOP;
 
-	/* Data to be written, and STOP after this. */
-	msgs[1].buf = data;
-	msgs[1].len = num_bytes;
-	msgs[1].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
-
-	return i2c_transfer(i2c_dev, &msgs[0], 2, ALS_ADDR);
+	return i2c_transfer(i2c_dev, &msg, 1, ALS_ADDR);
 }
 
-static int read_bytes(struct device *i2c_dev, u16_t addr,
-		      u8_t *data, u32_t num_bytes)
+/**
+ * @brief function to read a byte
+ * 
+ * @return 0 for success
+ */
+static int read_byte(struct device *i2c_dev, u8_t addr, u8_t *data)
 {
-	u8_t wr_addr;
 	struct i2c_msg msgs[2];
 
-	/* Now try to read back from FRAM */
-
-	/* FRAM address */
-	wr_addr = addr;
-
-	/* Setup I2C messages */
-
 	/* Send the address to read from */
-	msgs[0].buf = &wr_addr;
-	msgs[0].len = 2;
+	msgs[0].buf = &addr;
+	msgs[0].len = 1;
 	msgs[0].flags = I2C_MSG_WRITE;
 
 	/* Read from device. STOP after this. */
 	msgs[1].buf = data;
-	msgs[1].len = num_bytes;
+	msgs[1].len = 1;
 	msgs[1].flags = I2C_MSG_READ | I2C_MSG_STOP;
 
 	return i2c_transfer(i2c_dev, &msgs[0], 2, ALS_ADDR);
@@ -80,18 +65,15 @@ static int read_bytes(struct device *i2c_dev, u16_t addr,
 
 bool ALS_check_for_new_valid_data(struct device *i2c_dev)
 {
-  uint8_t status;
- 
-  read_bytes(i2c_dev, ALS_STATUS_REG, &status, 1);
+	uint8_t status;
 
-  if ((status & 0x04) && !(status & 0x80))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+	read_byte(i2c_dev, ALS_STATUS_REG, &status);
+
+	if ((status & 0x04) && ((status & 0x80) == 0)) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /**
@@ -101,21 +83,22 @@ bool ALS_check_for_new_valid_data(struct device *i2c_dev)
  */
 int ALS_get_integration_time(struct device *i2c_dev)
 {
-  int integration_time_lookup[8] = {100, 50, 200, 400, 150, 250, 300, 350};
+	int integration_time_lookup[8] = {
+		100, 50, 200, 400, 150, 250, 300, 350
+	};
 
-  uint8_t meas_rate_reg_data;
+	uint8_t meas_rate_reg_data;
 
-  if(read_bytes(i2c_dev, ALS_MEAS_RATE_REG, &meas_rate_reg_data, 1) != 0)
-  {
-	  LOG_ERR("get ch err");
-  }
-  
+	if (read_byte(i2c_dev, ALS_MEAS_RATE_REG, &meas_rate_reg_data) != 0) {
+		LOG_ERR("get ch err");
+	}
 
-  uint8_t integration_time_byte = (meas_rate_reg_data & 0x38) >> 3;
+	uint8_t integration_time_byte = (meas_rate_reg_data & 0x38) >> 3;
 
-  int als_integration_time_ms = integration_time_lookup[integration_time_byte];
+	int als_integration_time_ms =
+		integration_time_lookup[integration_time_byte];
 
-  return als_integration_time_ms;
+	return als_integration_time_ms;
 }
 
 /**
@@ -123,22 +106,24 @@ int ALS_get_integration_time(struct device *i2c_dev)
  * 
  * @return uint16_t* 
  */
-uint16_t* ALS_get_channels_data(struct device *i2c_dev)
+uint16_t *ALS_get_channels_data(struct device *i2c_dev)
 {
-  uint8_t als_buffer[4];
-  static uint16_t als_channels_data[2];
+	uint8_t als_buffer[4];
+	static uint16_t als_channels_data[2];
 
-  if(read_bytes(i2c_dev, ALS_DATA_CH1_0_REG, als_buffer, sizeof(als_buffer)) != 0)
-  {
-	  LOG_ERR("get ch err");
-  }
-  
-  als_channels_data[0] = (als_buffer[1] << 8) | als_buffer[0];
-  als_channels_data[1] = (als_buffer[3] << 8) | als_buffer[2];
+	for (int i = 0; i < 4; i++) {
+		if (read_byte(i2c_dev, ALS_DATA_CH1_0_REG + i,
+			      &als_buffer[i]) != 0) {
+			LOG_ERR("get ch err");
+		}
+	}
 
-  LOG_DBG("channels %d %d", als_channels_data[0], als_channels_data[1]);
+	als_channels_data[0] = (als_buffer[1] << 8) | als_buffer[0];
+	als_channels_data[1] = (als_buffer[3] << 8) | als_buffer[2];
 
-  return als_channels_data;
+	LOG_DBG("channels %d %d", als_channels_data[0], als_channels_data[1]);
+
+	return als_channels_data;
 }
 
 /**
@@ -148,18 +133,17 @@ uint16_t* ALS_get_channels_data(struct device *i2c_dev)
  */
 int ALS_get_gain(struct device *i2c_dev)
 {
-  int gain_lookup[8] = {1, 2, 4, 8, RESERVED, RESERVED, 48, 96};
+	int gain_lookup[8] = { 1, 2, 4, 8, RESERVED, RESERVED, 48, 96 };
 
-  uint8_t contr_reg_data;
-  read_bytes(i2c_dev, ALS_CONTR_REG, &contr_reg_data, sizeof(contr_reg_data));
+	uint8_t contr_reg_data;
+	read_byte(i2c_dev, ALS_CONTR_REG, &contr_reg_data);
 
-  uint8_t als_gain_byte = (contr_reg_data & 0x1C) >> 2;
+	uint8_t als_gain_byte = (contr_reg_data & 0x1C) >> 2;
 
-  int als_gain = gain_lookup[als_gain_byte];
+	int als_gain = gain_lookup[als_gain_byte];
 
-  return als_gain;
+	return als_gain;
 }
-
 
 /**
  * @brief calculate lux values from lux buffer
@@ -168,63 +152,85 @@ int ALS_get_gain(struct device *i2c_dev)
  */
 float ALS_get_lux(struct device *i2c_dev)
 {
-  uint16_t *als_channels_data;
+	uint16_t *als_channels_data;
 
-  als_channels_data = ALS_get_channels_data(i2c_dev);
+	als_channels_data = ALS_get_channels_data(i2c_dev);
 
-  //LOG_DBG("LTR  %x %x", als_channels_data[0], als_channels_data[1]);
+	//LOG_DBG("LTR  %x %x", als_channels_data[0], als_channels_data[1]);
 
-  float ratio = (float)als_channels_data[1]/(float)(als_channels_data[0]+als_channels_data[1]);
+	float ratio = (float)als_channels_data[1] /
+		      (float)(als_channels_data[0] + als_channels_data[1]);
 
-  int ALS_GAIN = ALS_get_gain(i2c_dev);
+	int ALS_GAIN = ALS_get_gain(i2c_dev);
 
-  float ALS_INT = (float)ALS_get_integration_time(i2c_dev)/100;
+	float ALS_INT = (float)ALS_get_integration_time(i2c_dev) / 100;
 
-  float als_lux;
+	float als_lux;
 
-  if(ratio<0.45)
-  {
-    als_lux = ((1.7743*(float)als_channels_data[0]) + (1.1059*(float)als_channels_data[1]))/(ALS_GAIN*ALS_INT);
-  }
-  else if(ratio<0.64 && ratio>=0.45)
-  {
-    als_lux = ((4.2785*(float)als_channels_data[0]) - (1.9548*(float)als_channels_data[1]))/(ALS_GAIN*ALS_INT);
-  }
-  else if(ratio<0.85 && ratio >=0.64)
-  {
-    als_lux = ((0.5926*(float)als_channels_data[0]) + (0.1185*(float)als_channels_data[1]))/(ALS_GAIN*ALS_INT);
-  }
-  else
-  {
-    als_lux = 0;
-  }
-
-  return als_lux;
+	if (ratio < 0.45) {
+		als_lux = ((1.7743 * (float)als_channels_data[0]) +
+			   (1.1059 * (float)als_channels_data[1])) /
+			  (ALS_GAIN * ALS_INT);
+	} else if (ratio < 0.64 && ratio >= 0.45) {
+		als_lux = ((4.2785 * (float)als_channels_data[0]) -
+			   (1.9548 * (float)als_channels_data[1])) /
+			  (ALS_GAIN * ALS_INT);
+	} else if (ratio < 0.85 && ratio >= 0.64) {
+		als_lux = ((0.5926 * (float)als_channels_data[0]) +
+			   (0.1185 * (float)als_channels_data[1])) /
+			  (ALS_GAIN * ALS_INT);
+	} else {
+		als_lux = 0;
+	}
+	return als_lux;
 }
 
-
+/**
+ * @brief fetch a sample from the sensor 
+ * 
+ * @return 0 for success and EAGAIN for fail
+ */
 static int ltr329als01_sample_fetch(struct device *dev,
 				    enum sensor_channel chan)
 {
-	struct ltr329als01_data *ltr_data = dev->driver_data; 
-	float als_val = ALS_get_lux(ltr_data->i2c_dev);
-	ltr_data->lux_val = als_val;
+	struct ltr329als01_data *ltr_data = dev->driver_data;
 
-	return 0;
+	if (chan == SENSOR_CHAN_ALL || chan == SENSOR_CHAN_LIGHT) {
+		if (ALS_check_for_new_valid_data(ltr_data->i2c_dev) == true) {
+			float als_val = ALS_get_lux(ltr_data->i2c_dev);
+			ltr_data->lux_val = als_val;
+			return 0;
+		}
+		else
+		{
+			return EAGAIN;
+		}
+		
+	} else {
+		return -ENOTSUP;
+	}
 }
 
+/**
+ * @brief sensor value get
+ * 
+ * @return 0 for success and EAGAIN for fail
+ */
 static int ltr329als01_channel_get(struct device *dev, enum sensor_channel chan,
-			   struct sensor_value *val)
+				   struct sensor_value *val)
 {
-	struct ltr329als01_data *ltr_data = dev->driver_data; 
-	
-    // TODO do this only on all channels or light channel
-	float als_val = ltr_data->lux_val;
-	val->val1 = (s32_t)als_val;
-	val->val2 = ((s32_t)(als_val * 1000000)) % 1000000;
-	//LOG_DBG("val1=%d, val2=%d", val->val1, val->val2);
+	struct ltr329als01_data *ltr_data = dev->driver_data;
 
-	return 0;
+	if (chan == SENSOR_CHAN_ALL || chan == SENSOR_CHAN_LIGHT) {
+		float als_val = ltr_data->lux_val;
+		val->val1 = (s32_t)als_val;
+		val->val2 = ((s32_t)(als_val * 1000000)) % 1000000;
+		//LOG_DBG("val1=%d, val2=%d", val->val1, val->val2);
+		return 0;
+
+	} else {
+		return -ENOTSUP;
+	}
 }
 
 static const struct sensor_driver_api ltr329als01_api = {
@@ -235,7 +241,9 @@ static const struct sensor_driver_api ltr329als01_api = {
 static int ltr329als01_init(struct device *dev)
 {
 	struct ltr329als01_data *drv_data = dev->driver_data;
-	uint8_t part_id = 0;
+	uint8_t manf_id = 0;
+
+	k_sleep(100); // wait after power on
 
 	LOG_INF("ltr329als01_init\r\n");
 
@@ -244,22 +252,28 @@ static int ltr329als01_init(struct device *dev)
 		LOG_ERR("i2c master not found.");
 		return -EINVAL;
 	}
-	
-	if(read_bytes(drv_data->i2c_dev, MANUFAC_ID_REG, &part_id, 1) == 0)
-	{
-		LOG_DBG("found! %x \r\n", part_id);
-	}
-	else
-	{
+
+	if (read_byte(drv_data->i2c_dev, MANUFAC_ID_REG, &manf_id) == 0) {
+		LOG_DBG("found! %x \r\n", manf_id);
+	} else {
 		return -EIO;
 	}
 
-	read_bytes(drv_data->i2c_dev, MANUFAC_ID_REG, &part_id, 1);
-	LOG_DBG("manf id=%x\r\n",part_id);
+	// set measurement and intergration time
+	uint8_t mea_int_time = 0x1b;
+	if (write_byte(drv_data->i2c_dev, ALS_MEAS_RATE_REG, mea_int_time) !=
+	    0) {
+		LOG_ERR("measurement and intergration time not set!");
+	}
 
-	//enable ltr sensor
+	/*enable ltr sensor - set to active mode  For Int time = 400ms, Meas 
+    rate = 500ms, Command = 0x1B*/
 	uint8_t enable_ltr = 0x01;
-	write_bytes(drv_data->i2c_dev,ALS_CONTR_REG, &enable_ltr,1);
+	if (write_byte(drv_data->i2c_dev, ALS_CONTR_REG, enable_ltr) != 0) {
+		LOG_ERR("not active");
+	}
+
+	k_sleep(10); // wait after active mode set
 
 	return 0;
 }
--- drivers/sensor/ltr329als01/ltr329als01.h
+++ drivers/sensor/ltr329als01/ltr329als01.h
@@ -1,5 +1,32 @@
+/** @file module.h
+ *
+ * @brief A description of the module’s purpose. 
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2018 Electronut Labs.
+ * All rights reserved. 
+*/
+#ifndef _LTR329ALS01_H
+#define _LTR329ALS01_H
+ 
+#ifdef __cplusplus 
+extern "C" { 
+#endif
 
+#include <sensor.h>
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <string.h>
+#include <misc/byteorder.h>
+#include <misc/__assert.h>
+#include <logging/log.h>
+#include <i2c.h>
 
+#define LOG_LEVEL CONFIG_SENSOR_LOG_LEVEL
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(ltr329als01);
 /* Light sensor address */
 #define ALS_ADDR 0x29
 
@@ -13,3 +40,84 @@
 #define ALS_DATA_CH0_0_REG 0x8A
 #define ALS_DATA_CH0_1_REG 0x8B
 #define ALS_STATUS_REG 0x8C
+
+#define RESERVED -1
+
+/**
+ * @brief function for write a byte
+ * 
+ * @return 0 for success
+ */
+static int write_byte(struct device *i2c_dev, u8_t addr, u8_t data);
+
+/**
+ * @brief function to read a byte
+ * 
+ * @return 0 for success
+ */
+static int read_byte(struct device *i2c_dev, u8_t addr, u8_t *data);
+
+/**
+ * @brief check for new data
+ * 
+ * @return true 
+ * @return false 
+ */
+bool ALS_check_for_new_valid_data(struct device *i2c_dev);
+
+/**
+ * @brief get integration time in ms from ALS_MEAS_RATE register
+ * 
+ * @return int 
+ */
+int ALS_get_integration_time(struct device *i2c_dev);
+
+/**
+ * @brief get adc values of channel-0 and channel-1 from als data registers
+ * 
+ * @return uint16_t* 
+ */
+uint16_t *ALS_get_channels_data(struct device *i2c_dev);
+
+/**
+ * @brief get gain values from ALS_CONTR register
+ * 
+ * @return int 
+ */
+int ALS_get_gain(struct device *i2c_dev);
+
+/**
+ * @brief calculate lux values from lux buffer
+ * 
+ * @return float 
+ */
+float ALS_get_lux(struct device *i2c_dev);
+
+/**
+ * @brief fetch a sample from the sensor 
+ * 
+ * @return 0 for success and EAGAIN for fail
+ */
+static int ltr329als01_sample_fetch(struct device *dev,
+				    enum sensor_channel chan);
+
+/**
+ * @brief sensor value get
+ * 
+ * @return 0 for success and EAGAIN for fail
+ */
+static int ltr329als01_channel_get(struct device *dev, enum sensor_channel chan,
+				   struct sensor_value *val);
+
+/**
+ * @brief initiasize the sensor
+ * 
+ * @return 0 for success 
+ */
+static int ltr329als01_init(struct device *dev);
+
+#ifdef __cplusplus 
+}
+#endif
+ 
+#endif /* _MODULE_H */
